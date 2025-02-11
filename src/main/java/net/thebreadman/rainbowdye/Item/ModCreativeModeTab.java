a639cc55cd480d4c02fe0f135fbb06e91d6945e6
dagobertdu94 <dagobertdu94@gmail.com>
2025-02-11 07:27:20 +0100

    Fix order of creative mode tabs

--- src/main/java/net/thebreadman/rainbowdye/Item/ModCreativeModeTab.java
+++ src/main/java/net/thebreadman/rainbowdye/Item/ModCreativeModeTab.java
@@ -10,7 +10,7 @@ import net.thebreadman.rainbowdye.block.ModBlocks;
 public class ModCreativeModeTab {
 	public static final DeferredRegister<CreativeModeTab> CREATIVE_MOD_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, RainbowDye.MOD_ID);
 	public static final RegistryObject<CreativeModeTab> TAB = CREATIVE_MOD_TABS.register("rainbow_dye_tab", () -> CreativeModeTab.builder()
-			.withTabsBefore(CreativeModeTabs.COMBAT).icon(() -> new ItemStack(ModItems.RAINBOW_DYE.get()))
+			.withTabsBefore(CreativeModeTabs.SPAWN_EGGS).icon(() -> new ItemStack(ModItems.RAINBOW_DYE.get()))
 			.displayItems((param, out) -> ModItems.ITEMS.getEntries().forEach((obj) -> obj.ifPresent((item) -> {
 				if (!(item instanceof BlockItem) || (item instanceof BlockItem bi && bi.getBlock() != ModBlocks.RAINBOW_CANDLE_CAKE.get())) {
 					out.accept(item);
